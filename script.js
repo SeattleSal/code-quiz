68057d83c9471d04c919e678ffb46d668648ce96
Sally Perez <seattlesally1@gmail.com>
2020-10-24 15:26:07 -0700

    added some basic css to style pages including button style

--- script.js
+++ script.js
@@ -9,3 +9,25 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and score
+
+// homepage has intro with a start button
+
+// when start button is pushed,
+// then start timer at 75 seconds and present question with options
+// when answer BUTTON is clicked
+// compare answer user chose to answer in questions array
+// if answer is a match
+//  ALERT correct and ????
+// if answer is not a match
+//  ALERT wrong! and subtract from time
+// IF timer == 0 OR all questions are answered
+//  then show All Done and score and text box for initials
+// when initials are added and SUBMIT button is pressed
+//  then go to high-scores.html
+
+// when go to high-scores.html
+//  if high-score array has values display in list each initial and score
+// when Go Back button is pressed
+//  display index.html
+// when Clear Highscores button is pressed,
+//  clear high-scores array
