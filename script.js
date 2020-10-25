a137bbf9dd2663fcab903c6bc3b36ead38b8bbf3
Sally Perez <seattlesally1@gmail.com>
2020-10-25 11:23:27 -0700

    added html to high-scores and tried to generate generic scores

--- script.js
+++ script.js
@@ -31,15 +31,22 @@
 // when Clear Highscores button is pressed,
 //  clear high-scores array
 
+// VARIABLES
 // grab elements needed such as start button
 var startButton = document.querySelector("#start");
 var mainEl = document.querySelector("main");
 var timeEl = document.querySelector("#timeLeft");
 
+// variables from high-scores.html
+var highScoresEl = document.querySelector("#highScores");
+var clearScoresButton = document.querySelector("#clear");
+
 var timeRemaining = 0;
 var score = 0;
 var questionIndex = 0;
+
 // TO DO: need variable for high scores, local storage?
+var highScores = localStorage.setItem("SRP", 1000);
 
 // storage for questions, possible answers and correct answer
 var questionsAndAnswers = [
@@ -62,8 +69,9 @@ var questionsAndAnswers = [
     }
 ] 
 
-console.log(questionsAndAnswers);
+// console.log(questionsAndAnswers);
 
+// FUNCTIONS
 // startQuiz
 // one by one, display each question and possible answers
 function startQuiz() {
@@ -114,8 +122,22 @@ function startTimer(){
     }, 1000);
 }
 
+// renderTime
+// print seconds on index.html in timer section
 function renderTime() {
     timeEl.textContent = timeRemaining;
 }
 
+function renderHighScores() {
+    highScoresEl.textContent = "Scores go here!";
+}
+
+// FUNCTION CALLS
+// Generate high scores page
+// TO DO - either this works or the listener works, how do i get both to work?
+// renderHighScores();
+
+// LISTENERS
 startButton.addEventListener("click", startQuiz);
+
+
