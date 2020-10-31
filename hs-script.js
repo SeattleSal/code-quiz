9731bb7fd979aab944567d3b9077f37c0db8fdf2
Sally Perez <seattlesally1@gmail.com>
2020-10-30 17:56:08 -0700

    high scores to render no, one or multiple high scores stored in local storage

--- hs-script.js
+++ hs-script.js
@@ -1,10 +1,3 @@
-// js file for high-scores page
-// when go to high-scores.html
-//  if high-score array has values display in list each initial and score
-// when Clear Highscores button is pressed,
-//  clear high-scores array
-
-
 // VARIABLES
 // variables from high-scores.html
 var highScoresEl = document.querySelector("#highScores");
@@ -13,56 +6,45 @@ var clearScoresButton = document.querySelector("#clear");
 // FUNCTIONS
 // renderHighScores
 // Retrieve the highScores stored in local storage and display
-// TO DO - figure out how to get values out of JSON?!?!?!?
-// THIS IS BROKEN!!!
 function renderHighScores() {
 
+    // if no scores are stored, show no scores stored message
     if (localStorage.getItem("highScores") === null) {
-        console.log("No scores stored");
+        highScoresEl.textContent = "No scores stored.";
     }
+    // if scores are stored, display the scores in a list
     else {
-        console.log("high scores are stored");
-        var x = localStorage.getItem("highScores");
-        console.log(x);
-        // var highScoresStored = JSON.parse(localStorage.getItem("highScores"));
-        // console.log("obj " + highScoresStored);
+        // there are scores stored
+        var hs = localStorage.getItem("highScores");
+        var hsObject = JSON.parse(hs);
+        console.log(hsObject);
+        var array = [];
 
-        // for(var key in highScoresStored) {
-        //    console.log(highScoresStored.initials + " " + highScoresStored.score);
-    //     initials = highScoresObject.initials;
-    //     score = highScoresObject.score;
-    //     console.log("HS Object" + highScoresObject + " " + typeof(highScoresObject));;
-    //     console.log("Initial and score" + initials + " " + score);
-    
-    //     highScoresEl.textContent = " " + initials + " - " + score;
-
-        // }
+        // create list elements
+        var listEl = document.createElement('ol');
+        highScoresEl.appendChild(listEl);
+        var liEl;
+        
+        if (hsObject.length) {
+            for(var x in hsObject) {
+                liEl = document.createElement("li");
+                liEl.innerHTML = "Initials: " + hsObject[x].initials + " Score: " + hsObject[x].score;
+                console.log(liEl.innerHTML);
+                listEl.appendChild(liEl);
+            }
+        } else {
+            console.log("theres only one score!");
+            liEl = document.createElement("li");
+            liEl.innerHTML ="Initials: " + hsObject.initials + " Score: " + hsObject.score;
+            listEl.appendChild(liEl);
+        }
     }
-
-    // get initials and scores from local storage
-    // console.log("HS String" + highScoresString + typeof(highScoresString));
-    // console.log(highScoresString);
-    // console.log("type of " + typeof(highScoreString));
-    // var highScoresObject = JSON.parse("["+highScoresString+"]");
-    // console.log("type? " + typeof(highScoresStored));
-    // var initials;
-    // var score;
-    // var key = "initials";
-
-    // console.log("initials?" + highScoresStored.initials);
-
-    // how do i get lenght to loop and what if there is only one?
-    // for all the initials and scores, render to screen
-    // for (i = 0; i < highScoresObject.length; i++) {
-
-    // console.log("Object stringify " + JSON.stringify(highScoresObject));
-
-
 }
 
 // clearScores when "Clear Scores" button is clicked
 clearScoresButton.addEventListener('click', function(e){
     localStorage.clear();
+    renderHighScores();
 })
 
 // FUNCTION CALLS
