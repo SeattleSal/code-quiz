ab2e89d6384edd5627930a7064dce07f38df880c
Sally Perez <seattlesally1@gmail.com>
2020-10-30 19:51:49 -0700

    Updated questions and answers to be about the Wizard of Oz

--- main-script.js
+++ main-script.js
@@ -1,16 +1,4 @@
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-
-// compare answer user chose to answer in questions array
-// if answer is a match
-//  ALERT correct and ????
-// if answer is not a match
-//  ALERT wrong! and subtract from time
-// IF timer == 0 OR all questions are answered
-//  then show All Done and score and text box for initials
-// when initials are added and SUBMIT button is pressed
-//  then go to high-scores.html
-
+// index.html for quiz game
 
 // VARIABLES
 // grab elements needed such as start button
@@ -21,28 +9,30 @@ var timeEl = document.querySelector("#timeLeft");
 // other variables for quiz functions
 var timeRemaining = 0;
 var score = 0; // do i need this variable?
-var startTime = 10; // 30 seconds for testing, switch to 75
-
-// TO DO: need variable for high scores, local storage?
-var highScoresObject = { 'playser1': 1, 'player2': 2, 'player3': 3 };
+var startTime = 60; // TO DO - change to 75 before turning in
 
 // storage for questions, possible answers and correct answer
 var questionsAndAnswers = [
     {   
-        question: "what?", 
-        possibleAnswers: ['yes', 'no', 'maybe'], 
-        rightAnswer: "yes"
+        question: "Who wrote the Wizard of Oz books?", 
+        possibleAnswers: ['Lewis Carroll', 'Roald Dahl', 'L. Frank Baum'], 
+        rightAnswer: 'L. Frank Baum'
     },
     {
-        question: "how?",
-        possibleAnswers: ['work', 'faith', 'luck'],
-        rightAnswer: 'work'
+        question: "How many Oz books are there?",
+        possibleAnswers: ['8', '20', '14'],
+        rightAnswer: '14'
 
     },
+    {   
+        question: "When was the first Oz book written?", 
+        possibleAnswers: ['1910', '1900', '1896'], 
+        rightAnswer: '1900'
+    },
     {
-        question: "The Invisible Man , a classic science fiction novel, was written by what author?",
-        possibleAnswers: ['Robert Louis Stevenson', 'H. G. Wells', 'Oscar Wilde'],
-        rightAnswer: 'H. G. Wells'
+        question: "In The Wizard of Oz, the Lion wanted to see the Wizard about getting..?",
+        possibleAnswers: ['Courage', 'A haircut', 'A heart'],
+        rightAnswer: 'Courage'
 
     },
     {
@@ -52,16 +42,27 @@ var questionsAndAnswers = [
 
     },
     {
-        question: "Don Quixote was written in which language?",
-        possibleAnswers: ['Spanish', 'Greek', 'Italian'],
-        rightAnswer: 'Spanish'
+        question: "In The Wizard of Oz, the Scarecrow wanted to see the Wizard about getting..?",
+        possibleAnswers: ['More straw', 'A brain', 'A heart'],
+        rightAnswer: 'A brain'
 
     },
     {
-        question: "Who is the heroine of Nathaniel Hawthorne’s The Scarlet Letter?",
-        possibleAnswers: ['Anne Hutchinson', 'Hester Prynne', 'Emily Shelby'],
-        rightAnswer: 'Hester Prynne'
+        question: "Dorothy and her friends fall asleep in a field of what kind of flower?",
+        possibleAnswers: ['Roses', 'Daisies', 'Poppies'],
+        rightAnswer: 'Poppies'
 
+    },
+    {
+        question: "What color are Dorothy's shoes in the book?",
+        possibleAnswers: ['Ruby', 'Gold', 'Silver'],
+        rightAnswer: 'Silver'
+
+    },
+    {
+        question: "At the end, whom did Dorothy say she’d miss most of all?",
+        possibleAnswers: ['Tin Man', 'Scarecrow', 'Lion'],
+        rightAnswer: 'Scarecrow'
     }
 ] 
 
@@ -73,7 +74,6 @@ var answerList;
 // startQuiz - one by one, display each question and possible answers
 function startQuiz() {
     var questionIndex = 0;
-    console.log("starting the quiz!");
 
     // start the timer
     startTimer();
@@ -84,6 +84,7 @@ function startQuiz() {
     answerList = document.createElement("ol");
 
     // render Questions and answers
+    // TO DO - should i put the for loop  here instead on in the renderQuestions function?
     renderQuestions(questionIndex);
 }
 
@@ -95,15 +96,16 @@ function renderQuestions(questionIndex) {
     answerList.innerText = "";
     var liEl;
     var button;
-    // console.log("clear main" + mainEl);
+    var result;
 
     // for the number of questions, display question and answers
-    if(questionIndex < 5){
+    if (questionIndex < questionsAndAnswers.length) {
         questionEl.innerText = questionsAndAnswers[questionIndex].question;
         mainEl.appendChild(questionEl);
 
         // create list of buttons with possible answers for each question
         for (var i = 0; i < questionsAndAnswers[questionIndex].possibleAnswers.length; i++) {
+            console.log("Creating answers for question " + questionIndex);
             // create <li> element with a data-index
             liEl = document.createElement("li");
             liEl.setAttribute("data-index", i);
@@ -116,36 +118,45 @@ function renderQuestions(questionIndex) {
         }
         mainEl.appendChild(answerList);
 
-        // When a element inside of the answers is clicked...
+        // When a answer button is clicked...
         answerList.addEventListener("click", function(event) {
-            var element = event.target;
             event.preventDefault();
-        
+            var element = event.target;
+            resultEl = document.createElement("p");
+
+            mainEl.appendChild(resultEl);
+                    
             // If that element is a button...
             // TO DO - check if answer is correct and change score, right now any button moves forward no difference
             if (element.matches("button") === true) {
                 // Get its data-index value from the button
-                var index = element.parentElement.getAttribute("data-index");
-                //  answerList.splice(index, 1);
-                console.log("button was pushed!" + index);
+                var answer = element.textContent;
+                console.log("answer: " + answer);
+                if(answer === questionsAndAnswers[questionIndex].rightAnswer){
+                    console.log("Correct!");
+                    resultEl = "Correct!";
+                    score++;
+                } else {
+                    console.log("wrong!");
+                    resultEl = "Wrong!";
+                    score--;
+                    timeRemaining -= 5;
+                }
+                console.log("Score is " + score);
+
                 questionIndex++;
                 renderQuestions(questionIndex);
-                // is answer correct? if so, print 'correct'; else print 'wrong', go to next question
             }
         });
-    } else { // reached end of questions
-        timeRemaining = 0;
+    } else {
         renderFinalScore(); // is this line redundant with code in startTimer?
     }
 }
 
 // startTimer
 function startTimer(){
-    // console.log("Starting timer!");
     timeRemaining = startTime;
     //setTime();
-    /* The "interval" variable here using "setInterval()" begins the recurring increment of the
-       secondsElapsed variable which is used to check if the time is up */
     interval = setInterval(function() {
         if (timeRemaining > 0) {
             // decrement by 1 second every second until time is up
@@ -161,7 +172,6 @@ function startTimer(){
 
 // renderFinalScore
 // create layout of game over and final score
-// TO DO - there has to be a shorter way to to all this...
 function renderFinalScore() {
     // clear page
     mainEl.innerHTML = "";
@@ -211,7 +221,6 @@ function renderFinalScore() {
             var array = [];
 
             if(hsObject.length) {
-                // console.log("HSLENTHG " + hsObject.length);
                 hsObject.map(item => array.push(item))
             } else {
                 array.push(hsObject);
@@ -223,7 +232,7 @@ function renderFinalScore() {
         }
 
         // navigate to high scores page
-        // window.location.href = "high-scores.html";
+        window.location.href = "high-scores.html";
 
     }
     )
